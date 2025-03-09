26e1427f469303fac47e964a1cbcd3b1c175ef67
Recio, Toni <mail@tonirecio.com>
2025-03-09 18:38:24 +0100

    vite standard setup

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+// commitlint.config.js
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "test",
+        "devops",        
+        "docs",
+        "style",
+        "refactor",
+      ],
+    ],
+  },
+};
